aab0c458eb7a4c7cb19e6f2c7d346a1f9b65ccb4
Sy Le <lenguyensy@gmail.com>
2018-08-24 09:13:55 -0700

    good start, pure js focus...

--- src/override/override.js
+++ src/override/override.js
@@ -10,6 +10,46 @@
             (e) => onUpdateBookmark(e.target.value.trim())
         )
 
+        // navigate results by keyboard
+        document.addEventListener('keydown',
+            (e) => {
+                const {key} = e;
+                let matches = [...document.querySelectorAll(`.match a`)];
+                let currentFocusedElem = document.querySelector(':focus');
+                const isTextBoxFocus = currentFocusedElem.id === 'txt-search';
+                const isUp = key === 'ArrowUp';
+                const isDown = key === 'ArrowDown';
+
+                if(matches.length === 0){
+                    return;
+                }
+
+                if(isUp || isDown){
+                    if(isTextBoxFocus){
+                        // focus on the first match
+                        currentFocusedElem = matches ? matches[0] : null;
+                    } else {
+                        const delta = isUp ? -1 : 1;
+                        let newTabIndex = currentFocusedElem.tabIndex + delta;
+
+                        // boundary
+                        newTabIndex = Math.min(matches.length + 1, newTabIndex);
+                        newTabIndex = Math.max(1, newTabIndex);
+
+                        // set the focus
+                        currentFocusedElem = matches[newTabIndex - 1];
+                    }
+
+
+                    // focus on it
+                    currentFocusedElem && currentFocusedElem.focus();
+
+                    // stop the scrolling
+                    e.preventDefault();
+                }
+            }
+        )
+
         // clean up
         const onUpdateBookmark = (function(){
             // debounce
@@ -21,7 +61,7 @@
                 timer = setTimeout(
                     function(){
                         const matches = searchBookmarks(keyword, flattened_bookmarks);
-                        populateBookmarks(matches);
+                        populateBookmarks(keyword, matches);
                     },
                     500
                 )
@@ -29,9 +69,12 @@
         })()
 
 
-        function populateBookmarks(matches){
+        function populateBookmarks(keyword, matches){
             let dom = '';
-            if(matches.length < 3){
+            if(keyword.length <= 2){
+                dom = '';// dont do anything for less than 2 chars..
+            }
+            else if(matches.length === 0){
                 dom = '<div class="no-match">No Matches</div>';
             } else {
                 matches.forEach(({url, title, breadcrumb}, idx) => {
@@ -39,13 +82,13 @@
                         dom += `<div class="match">
                             <span>${idx}. </span>
                             <strong>[${breadcrumb}]</strong>
-                            <a href="${url}">${title}</a>
+                            <a href="${url}" tabindex="${idx + 1}">${title}</a>
                         </div>`;
                     }
                     else {
                         dom += `<div class="match">
                             <span>${idx}. </span>
-                            <a href="${url}">${title}</a>
+                            <a href="${url}" tabindex="${idx + 1}">${title}</a>
                         </div>`;
                     }
                 })
