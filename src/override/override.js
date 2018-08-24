25d6e882b08e83e986eb910e7a9795c50e54aa12
Sy Le <lenguyensy@gmail.com>
2018-08-23 19:22:22 -0700

    show results

--- src/override/override.js
+++ src/override/override.js
@@ -25,29 +25,38 @@
                         const matches = searchBookmarks(keyword, flattened_bookmarks);
                         populateBookmarks(matches);
                     },
-                    1000
+                    500
                 )
             }
         })()
 
 
         function populateBookmarks(matches){
-            var dom;
-            console.log('matches', matches)
+            let dom = '';
+            if(matches.length < 3){
+                dom = '<div class="no-match">No Matches</div>';
+            } else {
+                matches.forEach(({url, title}) => {
+                    dom += `<div class="match"><a href="${url}">${title}</a></div>`;
+                })
+            }
 
             document.querySelector('#bookmarks-container')
                 .innerHTML = dom;
         }
 
         function searchBookmarks(keyword, flattened_bookmarks){
+            if(keyword.length === 0){
+                return [];
+            }
+
             return flattened_bookmarks.filter(
-                bookmark => fuzzyMatchBookmark(bookmark)
+                bookmark => fuzzyMatchBookmark(bookmark, keyword)
             )
         }
 
-        function fuzzyMatchBookmark({id, title, url}){
-            // TODO: cheat here with mod 119 (prime number)
-            return id % 97 === 0;
+        function fuzzyMatchBookmark({id, title, url}, keyword){
+            return title.indexOf(keyword) >= 0;
         }
 
 
